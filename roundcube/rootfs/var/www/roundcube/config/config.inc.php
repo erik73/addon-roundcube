e8c52c5fa19172e3eabd8c5ed681f1c3ab09495a
Erik Hilton <erikhilton73@gmail.com>
2025-06-30 15:02:01 +0200

    Add logging settings

--- roundcube/rootfs/var/www/roundcube/config/config.inc.php
+++ roundcube/rootfs/var/www/roundcube/config/config.inc.php
@@ -21,3 +21,15 @@ $config['des_key'] = 'MUU5FK7QJejl2Ng3Wg0z1pLd';
 $config['plugins'] = ['archive', 'managesieve', 'zipdownload'];
 
 $config['prefer_html'] = false;
+
+// Log successful/failed logins to <log_dir>/userlogins.log or to syslog
+$config['log_logins'] = true;
+
+// Log session debug information/authentication errors to <log_dir>/session.log or to syslog
+$config['session_debug'] = true;
+
+// Log SQL queries to <log_dir>/sql.log or to syslog
+$config['sql_debug'] = true;
+
+// Log IMAP conversation to <log_dir>/imap.log or to syslog
+$config['imap_debug'] = true;
