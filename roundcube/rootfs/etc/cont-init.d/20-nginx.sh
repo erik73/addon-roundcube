cae266815954861bda23636738f006efcfcdaf78
Erik Hilton <erikhilton73@gmail.com>
2025-07-01 17:12:04 +0200

    Add script file for Nginx

---
+++ roundcube/rootfs/etc/cont-init.d/20-nginx.sh
@@ -0,0 +1,20 @@
+#!/command/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Roundcube
+# This file configures nginx
+# ==============================================================================
+
+if bashio::var.is_empty "$(bashio::addon.port 80)"; then
+    bashio::log.warning "No host port is configured, please ensure a port is"
+    bashio::log.warning "set for external access to function"
+fi
+
+bashio::config.require.ssl
+bashio::var.json \
+    certfile "$(bashio::config 'certfile')" \
+    keyfile "$(bashio::config 'keyfile')" \
+    ssl "^$(bashio::config 'ssl')" \
+    | tempio \
+        -template /etc/nginx/templates/direct.gtpl \
+        -out /etc/nginx/servers/direct.conf
+
