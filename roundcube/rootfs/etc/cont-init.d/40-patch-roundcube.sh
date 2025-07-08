614a7b34771659137bbf1d4f5b8cd8b420f462c3
Ruben <ruben@rubend.nl>
2025-07-08 15:28:46 +0200

    Automatically patch roundcube when required.

--- roundcube/rootfs/etc/cont-init.d/40-patch-roundcube.sh
+++ roundcube/rootfs/etc/cont-init.d/40-patch-roundcube.sh
@@ -4,7 +4,7 @@
 # This file patches roundcube to disable renewal of the session cookie
 # ==============================================================================
 
-if bashio::config.true disableSessionRenewal; then
+if [[ $(bashio::supervisor.version) < "2025.07.2" ]] || [[ "$(bashio::core.version)" < "2025.07.2" ]]; then
   bashio::log.info "Patching Roundcube to disable session cookie renewal"
   sed -i 's/$RCMAIL->session->regenerate_id(false);//' /var/www/roundcube/index.php
 fi
